4d0fc951253e0f99262b223dca1d09b695575e60
Ondrej Viskup <viskup@pm.me>
2020-11-11 17:20:46 +0100

    landing page updates

--- assignement2/workout-app/src/app/navigation/landing-page/landing-page.component.ts
+++ assignement2/workout-app/src/app/navigation/landing-page/landing-page.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import {Workout} from "../../workout/workout.model";
+import {AuthService} from "../../core/auth/auth.service";
+import {ActivatedRoute} from "@angular/router";
 
 @Component({
   selector: 'app-landing-page',
@@ -9,19 +11,35 @@ import {Workout} from "../../workout/workout.model";
 export class LandingPageComponent implements OnInit {
   workouts: Workout[];
 
-  constructor() { }
+  constructor(private _authService: AuthService, private _activatedRoute: ActivatedRoute) { }
 
   ngOnInit(): void {
-    this.workouts = [
-      { _id: '1224fas', name: 'My workout 1', description: 'My first workout.', exercises: [
-        { name: 'Exercise 1', description: '', sets: 0, durationType: 'time', duration: 0 },
-          { name: 'Exercise 2', description: '', sets: 0, durationType: 'time', duration: 0 }
-        ] },
-      { _id: '1225fas', name: 'My workout 2', description: 'My second workout.', exercises: [] },
-      { _id: '1226fas', name: 'My workout 3', description: 'My third workout.', exercises: [] },
-      { _id: '1227fas', name: 'My workout 4', description: 'My fourth workout.', exercises: [] },
-      { _id: '1228fas', name: 'My workout 5', description: 'My fifth workout.', exercises: [] }
-    ];
+    this.workouts = this._activatedRoute.snapshot.data.workouts;
+    // this.workouts = [
+    //   { _id: '1224fas', name: 'My workout 1', description: 'My first workout.', exercises: [
+    //     { name: 'Exercise 1', description: '', sets: 0, durationType: 'time', duration: 0 },
+    //       { name: 'Exercise 2', description: '', sets: 0, durationType: 'time', duration: 0 }
+    //     ] },
+    //   { _id: '1225fas', name: 'My workout 2', description: 'My second workout.', exercises: [] },
+    //   { _id: '1226fas', name: 'My workout 3', description: 'My third workout.', exercises: [] },
+    //   { _id: '1227fas', name: 'My workout 4', description: 'My fourth workout.', exercises: [] },
+    //   { _id: '1228fas', name: 'My workout 5', description: 'My fifth workout.', exercises: [] }
+    // ];
   }
 
+  checkLoggedIn(): boolean {
+    return this._authService.isLoggedIn;
+  }
+
+  // TODO: remove when proper login form is done
+  login() {
+    this._authService.login({
+      email : "onj@example.com",
+      password : "qwerASDF"
+    })
+  }
+
+  logout() {
+    this._authService.logout();
+  }
 }
