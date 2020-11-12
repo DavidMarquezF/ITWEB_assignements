1e5b03448a1d4610d287143af1006419dcb8ddbf
David <dmarquez.estudis@gmail.com>
2020-11-12 14:15:34 +0100

    Workout log

---
+++ assignement2/workout_api/src/controllers/workout_log.js
@@ -0,0 +1,29 @@
+const WorkoutLog = require("../models/workout_log");
+const ObjectId = require('mongoose').Types.ObjectId;
+
+module.exports.getWorkoutLogs  = async function(req,res,next){
+    try{
+        const userWorkouts = await WorkoutLog.find().exec();
+        res.send(userWorkouts);    
+    }
+    catch(err){
+        next(err);
+    }
+}
+
+
+module.exports.createWorkoutLog = async function(req,res, next){
+    if(!req.body.timestamp || !req.body.workoutId){
+        return res.status(400).json({ "message": "Log needs a timestamp and a workoutId" });
+    }
+    else{
+        try{
+            const workout = new WorkoutLog({timestamp: req.body.timestamp, workoutId: req.body.workoutId, userId: req.user._id});
+            await workout.save();
+            res.send(workout);
+        }
+        catch(err){
+            return next(err);
+        }
+    }
+}
--- assignement2/workout_api/src/routes/api.js
+++ assignement2/workout_api/src/routes/api.js
@@ -3,9 +3,11 @@ const router = express.Router();
 
 const authRouter = require('./auth');
 const workoutRouter = require('./workout');
+const userRouter = require('./user')
 
 
 router.use("/workouts", workoutRouter);
 router.use("/auth", authRouter);
+router.use("/user", userRouter);
 
 module.exports = router;
