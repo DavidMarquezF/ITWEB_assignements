ec07ba0ef94af1a433251b04643756d9a01125da
unknown <aveho002@edu.xamk.fi>
2020-11-12 12:17:49 +0100

    Improved appearance on Register/Login

--- assignement2/workout-app/src/app/auth/login/login.component.ts
+++ assignement2/workout-app/src/app/auth/login/login.component.ts
@@ -15,7 +15,7 @@ export class LoginComponent implements OnInit {
   ngOnInit(): void {
   //  this.form = new FormGroup({
   //    email: new FormControl(null, Validators.required),
-  //    password: new FormControl(null, [Validators.required, Validators.minLength(6)]),
+  //    password: new FormControl(null, Validators.required),
   //  });
   }
 
