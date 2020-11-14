2a2b057553d9264978cf04b5e98a25fab883bf4a
Ondrej Viskup <viskup@pm.me>
2020-11-14 21:21:48 +0100

    Updated snackbar text

--- assignement2/workout-app/src/app/auth/register/register.component.ts
+++ assignement2/workout-app/src/app/auth/register/register.component.ts
@@ -1,11 +1,10 @@
-import { Component, Inject, OnInit, Optional } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { FormControl, FormGroup, Validators } from '@angular/forms';
 import { Router } from '@angular/router';
 import { catchError, concatMap } from 'rxjs/operators';
 import { AuthService } from '../../core/auth/auth.service';
 import { MatSnackBar } from '@angular/material/snack-bar';
 import { throwError } from 'rxjs';
-//import { User } from '../../core/auth/user.model';
 
 @Component({
   selector: 'app-register',
@@ -15,8 +14,6 @@ import { throwError } from 'rxjs';
 export class RegisterComponent implements OnInit {
 
   form: FormGroup;
-  //u : User;
-
 
   constructor(private _authService : AuthService, private _router: Router, private snackBar: MatSnackBar) { }
 
@@ -29,35 +26,33 @@ export class RegisterComponent implements OnInit {
 
   register(): void {
     if(this.form.invalid){
-      let snackBarRef = this.snackBar.open('Error upon registration. Check if the form is correct.', 'Close', {
-        duration: 5000
-      });
+      this.snackBar.open('Error upon registration. Check if the form is correct.','Close',
+        { duration: 5000 });
       return;
     }
     this._authService.register(this.form.value)
     .pipe(catchError(err => {
-      let snackBarRef = this.snackBar.open(`Error upon registration. ${err.error.message}.`, 'Close', {
-        duration: 5000
-      });
+      this.snackBar.open(`Error upon registration. ${err.error.message}.`, 'Close',
+        { duration: 5000 });
       return throwError(err);
     }),
-    concatMap(() => this._authService.login({email: this.form.value.email, password : this.form.value.password})),
+    concatMap(() => this._authService.login({email: this.form.value.email,
+      password : this.form.value.password})),
     catchError(err => {
-      let snackBarRef = this.snackBar.open(`Registration successful! Error upon logging in. ${err.error.message}.`, 'Close', {
-        duration: 5000
-      });
+      this.snackBar.open(`Registration unsuccessful! Error upon logging in. ${err.error.message}.`,
+        'Close', { duration: 5000 });
       return throwError(err);
     }))
     .subscribe(() => this._router.navigate(["/workouts"]));
   }
 
   getErrorEmail(){
-    return this.form.get("email").hasError("required") ? 
+    return this.form.get("email").hasError("required") ?
     "Field is required." : "This doesn't seem like a valid email address.";
   }
 
   getErrorPassword(){
-    return this.form.get("password").hasError("required") ? 
+    return this.form.get("password").hasError("required") ?
     "Field is required." : "Password needs to be at least six characters.";
   }
 
