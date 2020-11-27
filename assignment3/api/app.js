5f38b03718bb8476e24c2af28224a5e2dfaaa4a2
Ondrej Viskup <viskup@pm.me>
2020-11-27 11:50:40 +0100

    Created base API

--- assignment3/api/app.js
+++ assignment3/api/app.js
@@ -1,20 +1,48 @@
-var express = require('express');
-var path = require('path');
-var cookieParser = require('cookie-parser');
-var logger = require('morgan');
+require('dotenv').config();
+const express = require('express');
+const path = require('path');
+const cookieParser = require('cookie-parser');
+const logger = require('morgan');
+const cors = require('cors');
 
-var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
+require('./src/models/db');
 
-var app = express();
+const apiRouter = require('./src/routes/api');
+
+const app = express();
+
+app.use(cors());
 
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, 'src/public')));
 
-app.use('/', indexRouter);
-app.use('/users', usersRouter);
+app.use('/api', apiRouter);
+
+app.use(function(err, req,res,next) {
+  console.error(err);
+  next(err);
+});
+
+// Catch unauthorised errors
+app.use(function(err, req, res, next) {
+  if (err.name === 'UnauthorizedError') {
+    res.status(401);
+    res.json({ "message" : err.name + ": " + err.message});
+  } else {
+    next(err);
+  }
+});
+
+// error handler
+app.use(function(err, req, res, next) {
+  // set locals, only providing error in development
+  res.locals.message = err.message;
+  res.locals.error = req.app.get('env') === 'development' ? err : {};
+  // render the error page
+  res.status(err.status || 500).json({ "message": err.message })
+});
 
 module.exports = app;
